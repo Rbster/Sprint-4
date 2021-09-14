0bf065fd2f3ebc880338600281a9d96e5c04f217
Rbster <Rbster@yandex.ru>
2021-09-14 17:28:45 +0300

    functional: didn't test

--- functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
+++ functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
@@ -1,10 +1,31 @@
 package ru.sber.functional
 
+import java.util.function.Predicate
+
 class StudentsGroup {
 
-    lateinit var students: List<Student>
+    var students: List<Student> = init()
 
-    fun filterByPredicate() {
-        TODO()
+    fun filterByPredicate(predicate: (x: Student) -> Boolean): List<Student> {
+        val result = mutableListOf<Student>()
+        for (student in students) {
+            if (predicate(student)) {
+                result.add(student)
+            }
+        }
+        return result
+    }
+
+    fun init(): List<Student> {
+        val names = listOf("Alex", "Sergey", "Arnold", "Lucas", "Rachel", "Donald")
+        val surNames = listOf("Trump", "Donahue", "Andropov", "Parker", "Hegel", "Rasputin")
+        return listOf(Student(names[0], surNames[0], "", 0,4.3,"","",""),
+            Student(names[1], surNames[1], "", 0,4.3,"","",""),
+            Student(names[2], surNames[2], "", 0,5.0,"","",""),
+            Student(names[3], surNames[3], "", 0,1.4,"","",""),
+            Student(names[4], surNames[4], "", 0,3.6,"","",""),
+            Student(names[5], surNames[5], "", 0,2.1,"","",""),
+        )
+
     }
 }
