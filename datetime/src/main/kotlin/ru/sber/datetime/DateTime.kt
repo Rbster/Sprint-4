8ff2ddd33d10c97fa1d8dbf3534118d9719414f8
Rbster <Rbster@yandex.ru>
2021-09-16 17:18:24 +0300

    datetime: ok

--- datetime/src/main/kotlin/ru/sber/datetime/DateTime.kt
+++ datetime/src/main/kotlin/ru/sber/datetime/DateTime.kt
@@ -1,26 +1,46 @@
 package ru.sber.datetime
 
-import java.time.LocalDateTime
+import java.time.*
+import java.time.ZoneId.getAvailableZoneIds
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+import java.time.temporal.TemporalAccessor
+import java.time.temporal.TemporalAdjuster
+import java.time.temporal.TemporalAdjusters
+import java.util.*
 
 // 1.
-fun getZonesWithNonDivisibleByHourOffset(): Set<String> {
-    return emptySet()
-}
+fun getZonesWithNonDivisibleByHourOffset(): Set<String> = getAvailableZoneIds()
+    .asSequence()
+    .filter { ZonedDateTime.now(ZoneId.of(it)).minute != ZonedDateTime
+        .now(ZoneId.of("UTC")).minute }
+    .toSet()
 
 // 2.
-fun getLastInMonthDayWeekList(year: Int): List<String> {
-    return emptyList()
-}
+fun getLastInMonthDayWeekList(year: Int): List<String> = Month
+    .values()
+    .asSequence()
+    .map { LocalDateTime
+        .of(year, it, 1, 0, 0)
+        .with(TemporalAdjusters.lastDayOfMonth())
+        .dayOfWeek
+        .name }
+    .toList()
+
 
 // 3.
-fun getNumberOfFridayThirteensInYear(year: Int): Int {
-    return 0
-}
+fun getNumberOfFridayThirteensInYear(year: Int): Int = Month.values()
+    .asSequence()
+    .filter { LocalDateTime
+        .of(year, it, 13, 0, 0)
+        .dayOfWeek == DayOfWeek.FRIDAY }
+    .count()
 
 // 4.
-fun getFormattedDateTime(dateTime: LocalDateTime): String {
-    return ""
-}
+fun getFormattedDateTime(dateTime: LocalDateTime): String = dateTime
+    .format(DateTimeFormatter
+        .ofPattern("dd MMM yyyy, HH:mm")
+        .withLocale(Locale.US))
 
 
 
