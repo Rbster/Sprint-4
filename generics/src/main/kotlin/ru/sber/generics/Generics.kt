a78473202f75241fc92705d92016e1a60244b575
Rbster <Rbster@yandex.ru>
2021-09-16 15:06:50 +0300

    generics: ok

--- generics/src/main/kotlin/ru/sber/generics/Generics.kt
+++ generics/src/main/kotlin/ru/sber/generics/Generics.kt
@@ -1,7 +1,6 @@
 package ru.sber.generics
 
 
-
 // 1.
 fun <T1, T2> compare(p1: Pair<T1, T2>, p2: Pair<T1, T2>): Boolean {
     if (p1.first == p2.first && p1.second == p2.second)  {
@@ -9,28 +8,11 @@ fun <T1, T2> compare(p1: Pair<T1, T2>, p2: Pair<T1, T2>): Boolean {
     }
     return false
 }
-//fun <T1 : Comparable<T1>, T2: Comparable<T2>> compareComparable(p1: Pair<T1, T2>, p2: Pair<T1, T2>): Boolean {
-//    if (p1.first.compareTo(p2.first) == 0 && p1.second.compareTo(p2.second) == 0)  {
-//        return true
-//    }
-//    return false
-//}
-
 
 // 2.
-//fun countGreaterThan(anArray: Array<Any>, elem: Any): Int {
-//    return 0
-//}
 fun <T : Comparable<T>> countGreaterThan(anArray: Array<T>, elem: T) = anArray.asSequence().filter { it > elem }.count()
 
-
 // 3.
-//class Sorter {
-//    val list: MutableList
-//
-//    fun add(value: Any) {
-//    }
-//}
 class Sorter<T : Comparable<T>> {
     val list: MutableList<T> = mutableListOf()
 
@@ -51,7 +33,6 @@ class Sorter<T : Comparable<T>> {
 // 4.
 class Stack<T> {
     private class Node<T>(val prevNode: Node<T>?, val value: T?) {}
-
     private var top: Node<T> = Node(null, null)
 
     fun isEmpty(): Boolean {
@@ -73,11 +54,4 @@ class Stack<T> {
         }
         return null
     }
-
-//    fun popThrows(): T {
-//        if (!isEmpty()) {
-//            return top.value!!.also { top = top.prevNode ?: Node(null, null)}
-//        }
-//        throw IndexOutOfBoundsException()
-//    }
 }
